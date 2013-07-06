8f9c862be4ecb33128f89bf9fd19c6af72fc7350
Krassimir Boyadjiev <kboyad@gmail.com>
2013-07-06 06:16:07 +0000

    Load from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var buf     = require('buf');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  /*
   response.send('Hello World 2!');
+  */
+  mybuf = new Buffer(256);
+  len = mybuf.write(fs("./index.html"))
+  response.send(mybuf.toString('utf8',0,len));
 });
 
 var port = process.env.PORT || 5000;
